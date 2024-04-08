f55e9b6ce749ae703272fc3da3acf2dace8166d3
Roman <98784336+Animmant@users.noreply.github.com>
2024-04-08 07:51:52 +0000

    lection

---
+++ lection/04_service_car/service_car.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class car{
+    public:
+    
+    car(){
+
+    }
+    private:
+    string brand, model;
+    int year;
+    
+};
+
+class client{
+    public:
+    client(){
+
+    }
+private:
+    string name, adress, phone;
+};
+
+int main(){
+    car c1;
+    return 0;
+}

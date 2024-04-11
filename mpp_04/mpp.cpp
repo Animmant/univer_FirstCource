9d26fe14df1ac0b25e092efc3147688024521f8b
Roman <98784336+Animmant@users.noreply.github.com>
2024-04-11 07:52:43 +0000

    work_version

--- mpp_04/mpp.cpp
+++ mpp_04/mpp.cpp
@@ -3,18 +3,42 @@
 
 using namespace std;
 
-class clock{
+class Clock{
     public:
-    
-    car(){
-
+    Clock(int h = 0, int m = 0, int s = 0) : hour(h), minute(m), second(s) {}
+    void setAlarm(int h, int m, int s){
+        hour = h;
+        minute = m;
+        second = s;
     }
-    private:
-    string brand, model;
-    int year;
+    void addSecond(){
+        second += 1;
+    }
+
+    string getTime(){
+        string time = to_string(hour) + ":" + to_string(minute) + ":" + to_string(second);
+        return time;
+    }
+    protected:
+    int hour, minute, second;
     
 };
 
+class AlarmClock : public Clock {
+public:
+
+private:
+    int alarm_hour, alarm_minute, alarm_second;
+    bool military_format;
+};
+
+
 int main(){
+
+    Clock time_without_light(12, 35, 0);
+    string currentTime = time_without_light.getTime();
+    time_without_light.addSecond();
+    cout << time_without_light.getTime() << endl;
     return 0;
 }
+

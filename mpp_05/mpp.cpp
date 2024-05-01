a9fc74aec1b3ef4d77841ad931dab7642c4dc0b4
Roman <98784336+Animmant@users.noreply.github.com>
2024-05-01 10:45:21 +0000

    x

---
+++ mpp_05/mpp.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal{
+public:
+    Animal(const string& name) : name(name) {}
+    virtual void sound() const {
+        cout << "I am an animal" << endl;
+    }
+protected:
+string name;
+};
+class Dog : public Animal {
+public:
+Dog(const string& name) : Animal(name) {}
+    void voice() {
+        cout << "Woof woof" << endl;
+    }
+    void walk() {
+        cout << "The dog is going for a walk" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+Cat(const string& name) : Animal(name) {}
+    void voice() {
+        cout << "mauy" << endl;
+    }
+    void hunt() {
+        cout << "A cat hunts a mouse" << endl;
+    }
+};
+
+int main(){
+Dog myDog("Buddy");
+    myDog.voice(); myDog.walk();
+    Cat myCat("Whiskers");
+    myCat.voice();  myCat.hunt();  
+}
